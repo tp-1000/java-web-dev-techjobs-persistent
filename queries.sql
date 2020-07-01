2686c7231b5cf9a0e32c1d7d2a8f6bd146b5962e
thomaspetty <thomaskpetty@gmail.com>
2020-07-01 12:52:50 -0500

    Connect application with database
    -Necessary for data prescience features
    -Connects to MYSQL database through added dependencies
    -Data will contiue to exist after applcation ends

--- queries.sql
+++ queries.sql
@@ -1,4 +1,18 @@
 ## Part 1: Test it with SQL
+/* results ---
+id 			int PK
+employer 	varchar(255)
+name 		varchar(255)
+skills 		varchar(255)
+-------end */
+
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = "techjobs" and TABLE_NAME = "job";
+
+##or
+
+DESCRIBE job;
 
 ## Part 2: Test it with SQL
 
